183f7e2f10d9d069c8ff4f7121c9f2581b206e14
Kristanto Nugraha <knugroho150991@live.com>
2020-09-10 14:48:06 +0700

    Add project files.

---
+++ MarkomApp/Models/CreateUpdateModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarkomApp.Models
+{
+    public class CreateUpdate
+    {
+        public string Created_By { get; set; }
+        public DateTime Created_Date { get; set; }
+        public string Updated_By { get; set; }
+        public DateTime Updated_Date { get; set; }
+    }
+}
---
+++ MarkomApp/Models/UserModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarkomApp.Models
+{
+    public class User: CreateUpdate
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string M_Employee_Id { get; set; }
+
+    }
+}
